06de02393d9242942dde959240ebe8e324af67ad
Raphael Fiorda <raphaelfiorda@hotmail.com>
2022-01-11 19:23:55 -0300

    Requisito 18 concluído

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let email = document.querySelector('#email');
 let senha = document.querySelector('#senha');
-let botao = document.querySelector('#entrar')
+let botao = document.querySelector('#entrar');
+let agreement = document.getElementById('agreement');
+let botaoEnviar = document.getElementById('submit-btn');
 
 botao.addEventListener('click', validacao);
 
@@ -11,3 +13,14 @@ function validacao ( ) {
         alert('Email ou senha inválidos.')
     }
 }
+
+// Função que habilita o botão 'enviar' se o usuário concordar com o uso dos dados
+agreement.addEventListener('click', function submitControl(){
+    if (agreement.checked === true) {
+        botaoEnviar.removeAttribute('disabled')
+    } else {
+        botaoEnviar.setAttribute('disabled', true)
+    }
+})
+
+
