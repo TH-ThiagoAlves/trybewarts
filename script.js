28054138da5b4621899af3a1dcf80bb512573c1a
ThiagoTrybe19 <thiagoavels1@gmail.com>
2022-01-11 19:26:43 -0300

    Concerto de erro :rocket:

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-let email = document.querySelector('#email');
-let senha = document.querySelector('#senha');
-let botao = document.querySelector('#entrar')
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+const botao = document.querySelector('#entrar');
+const textarea = document.querySelector('#textarea');
+let counter = document.querySelector('#counter');
 
-botao.addEventListener('click', validacao);
-
-function validacao ( ) {
+function validacao () {
     if (email.value == 'tryber@teste.com' && senha.value == 123456) {
         return alert('Olá, Tryber!');
     }else {
         alert('Email ou senha inválidos.')
     }
 }
+
+function counterCaracter (textarea) {
+    let decrementar = 0;
+    console.log(textarea.target.innerText)
+    for (let i = 0; i < counter.target.innerText.length; i++) {
+        decrementar = decrementar + 1;
+    }
+    counter.innerText = parseInt(counter.innerText) - decrementar;
+    
+
+}
+
+
+textarea.addEventListener('keyup', counterCaracter);
+botao.addEventListener('click', validacao);
+
